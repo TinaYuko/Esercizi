ea7f560204ad0dfa51b618e7603d46d8332c28dd
Marti <Marti@DESKTOP-GANC7B7>
2021-10-05 14:19:33 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Esercizio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 
+
+            int contatore = 0;
+            Console.WriteLine("Scegliere una parola a piacere: ");
+
+            string parola = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Scegli una lettera da cercare!");
+            char lettera = Convert.ToChar(Console.ReadLine());
+
+            foreach (char l in parola)
+
+            {
+                if (l == lettera)
+
+                {
+                    contatore++;
+                }
+            }
+
+            if (contatore==0)
+
+            {
+                Console.WriteLine("La lettera scelta non è presente nella parola");
+            }
+            else
+            {
+                Console.WriteLine($"La lettera è presente {contatore} volte");
+            }
+            
+           
+
+            #endregion
+        }
+    }
+}
